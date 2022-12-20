3ed6745e47982123268e4a85c8579b6331473dc9
ExoskyCode <102408092+ExoskyCode@users.noreply.github.com>
2022-12-20 12:14:36 +0100

    fix

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,11 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
+      <h1 class="text-center mt-4">TODO-LIST</h1>
+      <Form />
     </div>
   );
 }
---
+++ react-app/src/components/Item.js
@@ -0,0 +1,11 @@
+
+export default function Item({txt}) {
+
+  return (
+    <li className="border d-flex justify-content-between align-items-center p-2 m-2">
+      <div className="p-3">{txt}</div>
+      <button className="btn btn-danger p-2 h-50">Supprimer</button>
+    </li>
+  );
+  
+}
