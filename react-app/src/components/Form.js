1e94a1b6f8115383cf154bc28db4869479f899c5
ExoskyCode <102408092+ExoskyCode@users.noreply.github.com>
2022-12-20 12:14:15 +0100

    Creating TODO LIST init && state

---
+++ react-app/src/components/Form.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function Form() {
+  const [dataArr, setDataArr] = useState([
+    { txt: "Promener le chien" },
+    { txt: "Apprendre React" },
+    { txt: "Apprendre Next" },
+    { txt: "Apprendre Node" },
+    { txt: "Apprendre Adonis" },
+  ]);
+
+  return (
+    <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
+      <form className="mb-3">
+        <label htmlFor="todo" className="form-label mt-3">
+          Choses à faire
+        </label>
+        <input type="text" className="form-control" id="todo" />
+        <button className="mt-2 btn btn-primary d-block">Envoyer</button>
+      </form>
+
+      <h2>Liste des choses à faire: </h2>
+      <ul className="list-group">
+        {dataArr.map((item, index) => {
+          return <Item txt={item.txt} key={index} />;
+        })}
+      </ul>
+    </div>
+  );
+}
